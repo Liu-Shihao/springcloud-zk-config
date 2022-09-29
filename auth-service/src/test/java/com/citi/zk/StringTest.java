b85cc6e23d08853598b69c4bdd52a8d14d2a361b
LiuShihao <liush99@foxmail.com>
2022-09-29 16:58:08 +0800

    splitSubPath

---
+++ auth-service/src/test/java/com/citi/zk/StringTest.java
@@ -0,0 +1,40 @@
+package com.citi.zk;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: LiuShihao
+ * @Date: 2022/9/29 16:28
+ * @Desc:
+ */
+@SpringBootTest
+public class StringTest {
+
+
+    @Test
+    public void test(){
+        String str = "/a/b/c";
+        List<String> strings = splitSubPath(str);
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+
+    public List<String> splitSubPath(String path){
+        ArrayList<String> ans = new ArrayList<>();
+        if (path == null){
+            return ans;
+        }
+        String[] split = path.split("/");
+        String subPath = "";
+        for (int i = 1; i < split.length; i++) {
+            subPath +="/"+split[i];
+            ans.add(subPath);
+        }
+        return ans;
+    }
+}
